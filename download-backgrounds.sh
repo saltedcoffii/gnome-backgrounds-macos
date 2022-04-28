da2a9d89d8370836fc9ca12c91675038c53732ed
Cassandra Watergate <55339220+saltedcoffii@users.noreply.github.com>
2022-04-27 19:20:27 -0700

    DRY download script

--- download-backgrounds.sh
+++ download-backgrounds.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
 set -e
 
-declare -A downloads=(
-  ['https://512pixels.net/downloads/macos-wallpapers/11-0-Night.jpg']='bigsur-d.jpg'
-  ['https://512pixels.net/downloads/macos-wallpapers/11-0-Day.jpg']='bigsur-l.jpg'
-  ['https://512pixels.net/downloads/macos-wallpapers/11-0-Big-Sur-Color-Night.jpg']='bigsur-color-d.jpg'
-  ['https://512pixels.net/downloads/macos-wallpapers/11-0-Color-Day.jpg']='bigsur-color-l.jpg'
-  ['https://512pixels.net/downloads/macos-wallpapers/10-15-Night.jpg']='catalina-d.jpg'
-  ['https://512pixels.net/downloads/macos-wallpapers/10-15-Day.jpg']='catalina-l.jpg'
-  ['https://512pixels.net/downloads/macos-wallpapers-6k/10-14-Night-6k.jpg']='mojave-d.jpg'
-  ['https://512pixels.net/downloads/macos-wallpapers-6k/10-14-Day-6k.jpg']='mojave-l.jpg'
-  ['https://512pixels.net/downloads/macos-wallpapers-6k/12-Dark.jpg']='monterey-d.jpg'
-  ['https://512pixels.net/downloads/macos-wallpapers-6k/12-Light.jpg']='monterey-l.jpg'
+_baseurl="https://512pixels.net/downloads/macos-wallpapers"
 
+declare -A downloads=(
+  ["$_baseurl/11-0-Night.jpg"]="bigsur-d.jpg"
+  ["$_baseurl/11-0-Day.jpg"]="bigsur-l.jpg"
+  ["$_baseurl/11-0-Big-Sur-Color-Night.jpg"]="bigsur-color-d.jpg"
+  ["$_baseurl/11-0-Color-Day.jpg"]="bigsur-color-l.jpg"
+  ["$_baseurl/10-15-Night.jpg"]="catalina-d.jpg"
+  ["$_baseurl/10-15-Day.jpg"]="catalina-l.jpg"
+  ["$_baseurl-6k/10-14-Day-6k.jpg"]="mojave-l.jpg"
+  ["$_baseurl-6k/10-14-Night-6k.jpg"]="mojave-d.jpg"
+  ["$_baseurl-6k/12-Dark.jpg"]="monterey-d.jpg"
+  ["$_baseurl-6k/12-Light.jpg"]="monterey-l.jpg"
 )
 
 for url in "${!downloads[@]}"; do
-  curl -L "${url}" -o $(dirname $0)/backgrounds/"${downloads[${url}]}";
+  curl -L "${url}" -o "$(dirname "$0")"/backgrounds/"${downloads[${url}]}";
 done
